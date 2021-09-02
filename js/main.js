dc154c805a038d0495ef41320c2be3ea2b26e98d
Mst-Akhi-Khatun <abiraajmi@gmail.com>
2021-09-02 18:56:01 +0600

    some-change

--- js/main.js
+++ js/main.js
@@ -1,51 +1,68 @@
+// taking all elements by id
 const displaySection = document.getElementById("display-section");
 const errorDiv = document.getElementById("error");
 const resultSection = document.getElementById("result");
 
-// Book loading arrow function
+// spinner
+const toggleSpinner = displayStyle => {
+  document.getElementById("spinner").style.display = displayStyle;
+}
+
+
+// Book loading by arrow function
 const loadBook = () => {
-    const searchInput = document.getElementById("search-input");
+  toggleSpinner('block');
+    const searchInput = document.getElementById("search-input");    
     const searchText = searchInput.value;
     
-    fetch(`http://openlibrary.org/search.json?q=${searchText}`)
+    // clear previous elements
+    displaySection.innerHTML = '';
+    resultSection.innerHTML = '';
+    searchInput.value = '';
+    errorDiv.value = '';
+    if (searchText === '') {
+      errorDiv.innerText = "Search field cannot be empty.";
+      return;
+    }
+    fetch(`https://openlibrary.org/search.json?q=${searchText}`)
     .then(res => res.json())
-    .then(data => displayBook(data.docs,data.numFound))
+    .then(data => displayBook(data.docs)) 
 }
 // loadBook();
 
 
 // Book display arrow function
-const displayBook = (books,result) => {
-  console.log(result);  
-  displaySection.innerHTML = '';
-
-    // Error Handing
-  if (books.status === 404) {
-    errorDiv.innerText = "NO Result Found";    
+const displayBook = (books) => {  
+  resultSection.innerHTML = `Total Search Results: ${books.length}`
+   
+    // Error Handling
+  if (books.length === 0) {
+    errorDiv.innerText = "NO Result Found";       
   } else {
     errorDiv.innerText = "";
   }
-    // console.log(books);
-    resultSection.innerText = '';
-    const h2 = document.createElement('h2');
-    h2.innerText = `Result are found ${result}`;       
-    resultSection.appendChild(h2);
-    books.forEach(book => {      
+   // single book element
+    books.forEach(book => {            
         const {title, first_publish_year, author_name, publisher, cover_i} = book;                  
-        const imageUrl = `https://covers.openlibrary.org/b/id/${cover_i}-M.jpg`        
-        const div = document.createElement('div');        
-        div.classList.add('col');
+        const imageUrl = `https://covers.openlibrary.org/b/id/${cover_i}-M.jpg`;
+        const defaultImg = 'images/Image-1.png';        
+        const div = document.createElement('div');             
         div.innerHTML = `        
-        <div class="card h-100">
-          <img src="${imageUrl}" class="card-img-top" alt="...">
-          <div class="card-body">
-            <h4>${title}</h4>
-            <h6>by ${author_name}</h6>
-            <h6>${publisher}</h6>
-            <p>First published in ${first_publish_year}</p>
-          </div>        
-        </div></br>
-        `
+        <div class="col h-75">
+            <div class="card h-100">
+                <img src="${cover_i ? imageUrl : defaultImg}" class="card-img-top h-50 p-1" alt="book image">
+                <div class="card-body overflow-auto pt-2">
+                    <h4 class="card-title">${title.slice(0, 30)}</h4>
+                    <h6 class="card-text"><strong>By:</strong> ${author_name ? author_name : 'no name found'}</h6>
+                    <h6 class="card-text"><strong>Publisher:</strong> ${publisher ? publisher[0] : 'no name found'}</h6>
+                    <h6 class="card-text">First published in <strong>${first_publish_year ? first_publish_year : '...'}</strong></h6>
+                </div>
+                <div class="card-footer bg-success py-2 text-center pt-1">
+                    <small class="text-white">Read Now</small>
+                </div>
+            </div>
+        </div>
+        `;
         displaySection.appendChild(div);
     })
 }

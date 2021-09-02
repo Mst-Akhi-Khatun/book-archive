e1d7023cf1bd04f9d4ae26d727529ac46841a678
Mst-Akhi-Khatun <abiraajmi@gmail.com>
2021-09-02 19:59:50 +0600

    done

--- js/main.js
+++ js/main.js
@@ -2,42 +2,46 @@
 const displaySection = document.getElementById("display-section");
 const errorDiv = document.getElementById("error");
 const resultSection = document.getElementById("result");
+const searchInput = document.getElementById("search-input");  
 
 // spinner
 const toggleSpinner = displayStyle => {
   document.getElementById("spinner").style.display = displayStyle;
 }
-
-
+// clear previous elements
+const emptyElement = () => {
+  displaySection.innerHTML = '';
+  resultSection.innerHTML = '';   
+  errorDiv.innerHTML = '';
+}
 // Book loading by arrow function
 const loadBook = () => {
-  toggleSpinner('block');
-    const searchInput = document.getElementById("search-input");    
-    const searchText = searchInput.value;
-    
-    // clear previous elements
-    displaySection.innerHTML = '';
-    resultSection.innerHTML = '';
-    searchInput.value = '';
-    errorDiv.value = '';
+  emptyElement();  //clear elements 
+  toggleSpinner('block'); //add spinner      
+  const searchText = searchInput.value;  
     if (searchText === '') {
-      errorDiv.innerText = "Search field cannot be empty.";
+      toggleSpinner('none'); 
+      errorDiv.innerHTML = `
+        <p class="text-center w-25 mx-auto p-2 bg-danger text-white">
+            Input field can't be empty!
+        </p>`;
       return;
     }
     fetch(`https://openlibrary.org/search.json?q=${searchText}`)
     .then(res => res.json())
-    .then(data => displayBook(data.docs)) 
+    .then(data => displayBook(data.docs))
+    searchInput.value = ''; 
 }
-// loadBook();
-
-
-// Book display arrow function
-const displayBook = (books) => {  
-  resultSection.innerHTML = `Total Search Results: ${books.length}`
-   
+// display Book arrow function
+const displayBook = (books) => {
+  toggleSpinner('none');  
+  resultSection.innerHTML = `Total Search Results: ${books.length}`   
     // Error Handling
   if (books.length === 0) {
-    errorDiv.innerText = "NO Result Found";       
+    errorDiv.innerHTML = `
+    <p class="text-center w-25 mx-auto p-2 bg-danger text-white">
+        NO RESULTS FOUND!
+    </p>`;       
   } else {
     errorDiv.innerText = "";
   }
@@ -52,7 +56,7 @@ const displayBook = (books) => {
             <div class="card h-100">
                 <img src="${cover_i ? imageUrl : defaultImg}" class="card-img-top h-50 p-1" alt="book image">
                 <div class="card-body overflow-auto pt-2">
-                    <h4 class="card-title">${title.slice(0, 30)}</h4>
+                    <h4 class="card-title">${title}</h4>
                     <h6 class="card-text"><strong>By:</strong> ${author_name ? author_name : 'no name found'}</h6>
                     <h6 class="card-text"><strong>Publisher:</strong> ${publisher ? publisher[0] : 'no name found'}</h6>
                     <h6 class="card-text">First published in <strong>${first_publish_year ? first_publish_year : '...'}</strong></h6>
